ac12ff56378fead37f0c726f1f5aaa7d0aaffe4a
medhaouari7 <medhaouari025@gmail.com>
2022-03-09 11:26:33 +0100

    small cards

--- airbnb/pages/index.js
+++ airbnb/pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCrad from '../components/SmallCrad';
 
 export default function Home( {exploreData} ) {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>AIRBNB</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,7 +15,10 @@ export default function Home( {exploreData} ) {
     <main className="max-w-7xl mx-auto px-8 sm:px-16">
       <secction className="pt-6">
         <h2 className="text-4xl">Explore Nearby</h2>
-        {exploreData.map((item) => (<h1>{item.location}</h1>))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {exploreData?.map(({img , distance , location }) => (<SmallCrad key={img} img={img} distance={distance} location={location} />))}
+        </div>
+        
         </secction>
     </main>
 
