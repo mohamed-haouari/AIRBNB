78fbbd3e3f7f4421850fc34bd17080f41a7a6a2d
medhaouari7 <medhaouari025@gmail.com>
2022-03-09 12:07:16 +0100

    medium cards

--- airbnb/pages/index.js
+++ airbnb/pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard';
 import SmallCrad from '../components/SmallCrad';
 
-export default function Home( {exploreData} ) {
+export default function Home( {exploreData , cardData } ) {
   return (
     <div>
       <Head>
@@ -13,13 +14,20 @@ export default function Home( {exploreData} ) {
     <Header/>
     <Banner/>
     <main className="max-w-7xl mx-auto px-8 sm:px-16">
-      <secction className="pt-6">
+      <section className="pt-6">
         <h2 className="text-4xl">Explore Nearby</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {exploreData?.map(({img , distance , location }) => (<SmallCrad key={img} img={img} distance={distance} location={location} />))}
         </div>
-        
-        </secction>
+        </section>
+
+        <section>
+         <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+         <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+         {cardData?.map(({img ,title }) => (<MediumCard key={img} img={img} title={title} />))}
+         </div>
+        </section>
+
     </main>
 
     </div>
@@ -28,9 +36,11 @@ export default function Home( {exploreData} ) {
 
 export async function getStaticProps() {
 const exploreData = await fetch('https://links.papareact.com/pyp').then((res)=> res.json());
+const cardData = await fetch('https://links.papareact.com/zp1').then((res)=> res.json());
  return {
    props: {
     exploreData,
+    cardData
    } 
  }
 }
