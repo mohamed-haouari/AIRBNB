ed1d59df2417c684eee53ac71cea0025dcd442bd
medhaouari7 <medhaouari025@gmail.com>
2022-03-09 16:57:57 +0100

    calendar picker

--- airbnb/components/Header.js
+++ airbnb/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import Image from 'next/image';
 import { GlobeAltIcon,MenuIcon,UserCircleIcon,UsersIcon,SearchIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
 
 function Header() {
+const  [searchInput , setSearchInput ] = useState("");
+const [startDate , setStartDate ] = useState(new Date());
+const [endDate , setendDate ] = useState(new Date());
+
+const handleSelect = (ranges) => {
+  setStartDate (ranges.selection.startDate);
+  setendDate (ranges.selection.endDate);
+};
+
+const selectionRange = {
+  startDate : startDate,
+  endDate : endDate,
+  key: "selection",
+};
+
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
        <div className='relative flex-items h-10 cursor-pointer my-auto'>
@@ -14,7 +35,7 @@ function Header() {
        </div>
 
        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-           <input className="flex-grow pl-5 bg-transparent outline-none md:mx-2 text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Tap to search"  />
+           <input value={searchInput} onChange = {(e) => setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none md:mx-2 text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Tap to search"  />
            <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer"/>
        </div>
 
@@ -27,6 +48,9 @@ function Header() {
            <UserCircleIcon className="h-6" />
            </div>
        </div>
+       {searchInput && <div className="flex flex-col col-span-3 mx-auto"><DateRangePicker ranges={[selectionRange]} minDate={new Date()} rangeColors={["#FD5B61"]} onChange={handleSelect}/> 
+       <div><h2 className="text-2xl flex-grow">Number of Guests</h2></div>
+       </div>}
     </header>
   )
 }
