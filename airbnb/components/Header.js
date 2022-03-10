0974c6da907986df99a54f6d93eb7dcd419668d8
medhaouari7 <medhaouari025@gmail.com>
2022-03-10 12:23:13 +0100

    search page

--- airbnb/components/Header.js
+++ airbnb/components/Header.js
@@ -5,13 +5,14 @@ import { useState } from 'react';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
+import {useRouter} from 'next/dist/client/router';
 
-
-function Header() {
+function Header({placeholder}) {
 const  [searchInput , setSearchInput ] = useState("");
 const [startDate , setStartDate ] = useState(new Date());
 const [endDate , setendDate ] = useState(new Date());
 const [noOfGuests , setnoOfGuests ] = useState(1);
+const router = useRouter();
 
 
 const handleSelect = (ranges) => {
@@ -23,6 +24,18 @@ const resetInput = () => {
 setSearchInput ("")
 }
 
+const search = () => {
+  router.push({
+    pathname: "/search",
+    query: {
+      location : searchInput,
+      startDate : startDate.toISOString(),
+      endDate : endDate.toISOString(),
+      noOfGuests,
+    },
+  });
+}
+
 const selectionRange = {
   startDate : startDate,
   endDate : endDate,
@@ -32,7 +45,7 @@ const selectionRange = {
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-       <div className='relative flex-items h-10 cursor-pointer my-auto'>
+       <div onClick={() => router.push("/")} className='relative flex-items h-10 cursor-pointer my-auto'>
        <Image  src="https://links.papareact.com/qd3"   
        layout="fill"  
        objectFit="contain"
@@ -41,7 +54,7 @@ const selectionRange = {
        </div>
 
        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-           <input value={searchInput} onChange = {(e) => setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none md:mx-2 text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Tap to search"  />
+           <input value={searchInput} onChange = {(e) => setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none md:mx-2 text-sm text-gray-600 placeholder-gray-400" type="text" placeholder={placeholder || "Tap to search"}  />
            <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer"/>
        </div>
 
@@ -62,7 +75,7 @@ const selectionRange = {
          </div>
          <div className="flex">
            <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
-           <button className="flex-grow text-red-400">Search</button>
+           <button onClick={search} className="flex-grow text-red-400">Search</button>
          </div>
        </div>}
     </header>
